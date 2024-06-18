448119d6ab2ac98197ac96b37ba6a1d8e90a8a9b
dada416-lebg <yanis.fronteau@gmail.com>
2024-06-18 22:19:06 +0200

    server.go
    basic fonction for the server

---
+++ internal/app-server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	store  store.Store
+	router *http.ServeMux
+	logger *log.Logger
+}
+
+func NewServer(store store.Store) *server {
+	return &server{
+		store:  store,
+		router: &http.ServeMux{},
+		logger: log.Default(),
+	}
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func Start(con Config) error {
+	db, err := InitDB(con)
+	if err != nil {
+		return err
+	}
+
+	store := sqlite.NewSQL(db)
+
+	server := NewServer(store)
+
+	server.HandlePaths()
+
+	log.Println("Starting server: http://localhost:8080")
+
+	return http.ListenAndServe(con.Port, server)
+}
