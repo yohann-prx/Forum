593dd1efedc1d49d9619d88077692101602ed1f0
Yohann Prieux <tanitay93140@gmail.com>
2024-06-19 14:39:23 +0200

    add reaction struct

--- internal/model/reaction.go
+++ internal/model/reaction.go
@@ -1,25 +1,11 @@
 package model
 
-type ReactionType string
-
-const (
-	Like    ReactionType = "like"
-	Dislike ReactionType = "dislike"
-)
+import "time"
 
 type Reaction struct {
-	UserID     string       `json:"user_id"`
-	PostID     string       `json:"post_id,omitempty"`
-	CommentID  string       `json:"comment_id,omitempty"`
-	ReactionID int          `json:"reaction_id"`
-	Type       ReactionType `json:"type"`
-}
-
-func NewReaction(userID, postID, commentID string, reactionType ReactionType) *Reaction {
-	return &Reaction{
-		UserID:    userID,
-		PostID:    postID,
-		CommentID: commentID,
-		Type:      reactionType,
-	}
+	ID        int
+	UserID    int
+	PostID    int
+	Emoji     string
+	CreatedAt time.Time
 }
