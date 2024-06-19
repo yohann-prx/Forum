f1db9555145f5c4ff67bcba0cd350ffa8e1229f0
dada416-lebg <yanis.fronteau@gmail.com>
2024-06-19 19:18:47 +0200

    user  uf fix

--- internal/store/sqlite/userrepository.go
+++ internal/store/sqlite/userrepository.go
@@ -2,7 +2,6 @@ package sqlite
 
 import (
 	"SPORTALK/internal/model"
-	"database/sql"
 	"errors"
 	"fmt"
 
@@ -14,105 +13,60 @@ type UserRepository struct {
 }
 
 func (r *UserRepository) ExistingUser(userName, email string) error {
-	tx, err := r.store.Db.Begin()
+	queryEmail := "SELECT * FROM users WHERE email = ?"
+	rows, err := r.store.Db.Query(queryEmail, email)
 	if err != nil {
-		return fmt.Errorf("transaction start failed: %v", err)
-	}
-	defer tx.Rollback()
-
-	// Vérification de l'email
-	queryEmail := "SELECT 1 FROM users WHERE email = ? LIMIT 1"
-	var emailExists int
-	err = tx.QueryRow(queryEmail, email).Scan(&emailExists)
-	if err != nil && err != sql.ErrNoRows {
 		return fmt.Errorf("email check failed: %v", err)
 	}
-	if emailExists == 1 {
+	defer rows.Close()
+
+	if rows.Next() {
 		return errors.New("email already in use")
 	}
 
-	// Vérification du nom d'utilisateur
-	queryName := "SELECT 1 FROM users WHERE username = ? LIMIT 1"
-	var usernameExists int
-	err = tx.QueryRow(queryName, userName).Scan(&usernameExists)
-	if err != nil && err != sql.ErrNoRows {
-		return fmt.Errorf("username check failed: %v", err)
-	}
-	if usernameExists == 1 {
-		return errors.New("username already in use")
-	}
-
-	// Commit la transaction si tout est OK
-	err = tx.Commit()
+	queryName := "SELECT * FROM users WHERE username = ?"
+	rows, err = r.store.Db.Query(queryName, userName)
 	if err != nil {
-		return fmt.Errorf("transaction commit failed: %v", err)
+		return fmt.Errorf("user name check failed: %v", err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		return errors.New("username already in use")
 	}
 
 	return nil
 }
 
 func (r *UserRepository) Login(user *model.User) error {
-	var storedUUID string
-	var storedEmail string
-	var storedUserName string
-	var storedHashedPassword string
-
-	// Récupérer les informations utilisateur depuis la base de données
-	err := r.store.Db.QueryRow("SELECT UUID, email, username, password FROM users WHERE email = ?", user.Email).
-		Scan(&storedUUID, &storedEmail, &storedUserName, &storedHashedPassword)
+	var hashedPassword string
+	err := r.store.Db.QueryRow("SELECT UUID, email, username, password FROM users WHERE email = ?", user.Email).Scan(&user.UUID, &user.Email, &user.UserName, &hashedPassword)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return errors.New("user not found")
-		}
-		return fmt.Errorf("error fetching user data: %v", err)
+		return err
 	}
-
-	// Comparer le mot de passe hashé stocké avec celui fourni par l'utilisateur
-	err = bcrypt.CompareHashAndPassword([]byte(storedHashedPassword), []byte(user.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password))
 	if err != nil {
-		return errors.New("invalid password")
+		return err
 	}
-
-	// Mettre à jour les données utilisateur avec celles récupérées de la base de données
-	user.UUID = storedUUID
-	user.Email = storedEmail
-	user.UserName = storedUserName
-
 	return nil
 }
 
 func (r *UserRepository) Register(user *model.User) error {
-	// Hasher le mot de passe
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	queryInsert := "INSERT INTO users(UUID, email, username, password) VALUES(?, ?, ?, ?) "
+	_, err := r.store.Db.Exec(queryInsert, user.UUID, user.Email, user.UserName, user.Password)
 	if err != nil {
-		return fmt.Errorf("password hashing failed: %v", err)
+		return err
 	}
-
-	// Préparer la requête d'insertion
-	queryInsert := "INSERT INTO users(UUID, email, username, password) VALUES(?, ?, ?, ?)"
-	stmt, err := r.store.Db.Prepare(queryInsert)
-	if err != nil {
-		return fmt.Errorf("insert statement preparation failed: %v", err)
-	}
-	defer stmt.Close()
-
-	// Exécuter l'insertion avec les valeurs hashées du mot de passe
-	_, err = stmt.Exec(user.UUID, user.Email, user.UserName, hashedPassword)
-	if err != nil {
-		return fmt.Errorf("insert execution failed: %v", err)
-	}
-
 	return nil
 }
 
+// Gets a user by UUID
 func (r *UserRepository) GetByUUID(uuid string) (*model.User, error) {
 	var u model.User
-	err := r.store.Db.QueryRow(
-		"SELECT UUID, username, email FROM users WHERE UUID = ?",
+	if err := r.store.Db.QueryRow(
+		"SELECT UUID, username, email, password FROM users WHERE UUID = ?",
 		uuid,
-	).Scan(&u.UUID, &u.UserName, &u.Email)
-
-	if err != nil {
+	).Scan(&u.UUID, &u.UserName, &u.Email, &u.Password); err != nil {
 		return nil, err
 	}
 
