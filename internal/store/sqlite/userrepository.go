fb5e792bf673606bc32c4c14689d4320a2344fa1
dada416-lebg <yanis.fronteau@gmail.com>
2024-06-19 19:16:04 +0200

    login logic

--- internal/store/sqlite/userrepository.go
+++ internal/store/sqlite/userrepository.go
@@ -1,9 +1,12 @@
 package sqlite
 
 import (
+	"SPORTALK/internal/model"
 	"database/sql"
 	"errors"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepository struct {
@@ -44,6 +47,36 @@ func (r *UserRepository) ExistingUser(userName, email string) error {
 	if err != nil {
 		return fmt.Errorf("transaction commit failed: %v", err)
 	}
+
+	return nil
+}
+
+func (r *UserRepository) Login(user *model.User) error {
+	var storedUUID string
+	var storedEmail string
+	var storedUserName string
+	var storedHashedPassword string
+
+	// Récupérer les informations utilisateur depuis la base de données
+	err := r.store.Db.QueryRow("SELECT UUID, email, username, password FROM users WHERE email = ?", user.Email).
+		Scan(&storedUUID, &storedEmail, &storedUserName, &storedHashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return errors.New("user not found")
+		}
+		return fmt.Errorf("error fetching user data: %v", err)
+	}
+
+	// Comparer le mot de passe hashé stocké avec celui fourni par l'utilisateur
+	err = bcrypt.CompareHashAndPassword([]byte(storedHashedPassword), []byte(user.Password))
+	if err != nil {
+		return errors.New("invalid password")
+	}
+
+	// Mettre à jour les données utilisateur avec celles récupérées de la base de données
+	user.UUID = storedUUID
+	user.Email = storedEmail
+	user.UserName = storedUserName
 
 	return nil
 }
