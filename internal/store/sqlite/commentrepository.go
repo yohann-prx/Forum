c370548bc5dccab54b44c205720a15f4b2758faa
dada416-lebg <yanis.fronteau@gmail.com>
2024-06-19 15:25:19 +0200

    commentary logic

--- internal/store/sqlite/commentrepository.go
+++ internal/store/sqlite/commentrepository.go
@@ -1,6 +1,6 @@
 package sqlite
 
-import "Forum/internal/model"
+import "SPORTALK/internal/model"
 
 type CommentRepository struct {
 	store *Store
@@ -13,4 +13,39 @@ func (r *CommentRepository) Create(c *model.Comment) error {
     `, c.ID, c.PostID, c.UserID, c.Content)
 
 	return err
+}
+
+func (r *CommentRepository) GetCommentsWithReactionsByPostID(postID string) ([]*model.Comment, error) {
+	query := `
+    SELECT c.id, c.post_id, c.user_uuid, c.content, c.created_at,
+           COALESCE(SUM(CASE WHEN cr.reaction_id = 1 THEN 1 ELSE 0 END), 0) AS LikeCount,
+           COALESCE(SUM(CASE WHEN cr.reaction_id = 2 THEN 1 ELSE 0 END), 0) AS DislikeCount
+	FROM comments c
+	LEFT JOIN comment_reactions cr ON c.id = cr.comment_id
+	WHERE c.post_id = ?
+	GROUP BY c.id`
+	rows, err := r.store.Db.Query(query, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var comments []*model.Comment
+
+	for rows.Next() {
+		var comment model.Comment
+		err = rows.Scan(&comment.ID, &comment.PostID, &comment.UserID, &comment.Content, &comment.CreatedAt, &comment.LikeCount, &comment.DislikeCount)
+		if err != nil {
+			return nil, err
+		}
+		user, err := r.store.User().GetByUUID(comment.UserID)
+		if err != nil {
+			return nil, err
+		}
+		comment.User = user
+
+		comments = append(comments, &comment)
+	}
+
+	return comments, nil
 }
